76ec8fa216438baeec5997f361abbfc63cfc6e1f
Herman Schmidt <hermanschmidt@gmail.com>
2020-01-25 21:35:03 +0000

    fixes

---
+++ js/content.js
@@ -0,0 +1,36 @@
+const content = [
+  {
+    id: "brandsAndOrganizations-automotiveAndTransportation-airline",
+    label: "Airline"
+  },
+  {
+    id: "brandsAndOrganizations-automotiveAndTransportation-carManufacturer",
+    label: "Car Manufacturer"
+  },
+  {
+    id: "brandsAndOrganizations-automotiveAndTransportation-truckManufacturer",
+    label: "Truck Manufacturer"
+  },
+  {
+    id: "brandsAndOrganizations-automotiveAndTransportation-other",
+    label: "Other"
+  },
+  { id: "brandsAndOrganizations-community-example1", label: "Example 1" },
+  { id: "brandsAndOrganizations-community-example2", label: "Example 2" },
+  {
+    id: "brandsAndOrganizations-consumerElectronics-example1",
+    label: "Example 1"
+  },
+  {
+    id: "brandsAndOrganizations-consumerElectronics-example2",
+    label: "Example 2"
+  },
+  { id: "brandsAndOrganizations-fashion-clothing", label: "Clothing" },
+  { id: "brandsAndOrganizations-fashion-footwear", label: "Footwear" },
+  { id: "brandsAndOrganizations-fashion-jewelry", label: "Jewelry" },
+  { id: "brandsAndOrganizations-fashion-watches", label: "Watches" },
+  { id: "media-example1", label: "Example 1" },
+  { id: "media-example2", label: "Example 2" },
+  { id: "influencer-example1", label: "Example 1" },
+  { id: "influencer-example2", label: "Example 2" }
+];
